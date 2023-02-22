5301eca5c5d6a1c6466d03d56b196e727bdc7342
Susanna Caroppo <sus.caroppo@stud.uniroma3.it>
2023-02-22 09:56:19 +0000

    more examples

--- tutte_method.py
+++ tutte_method.py
@@ -82,7 +82,7 @@ def build_cqm(vars: Variables, g: nx.Graph(), fixed_points: list, upperBound: in
     _define_objective(cqm, vars, g, no_nodes)
     
     
-    #print(cqm.objective)
+    print(cqm.objective)
     
     return cqm
 
@@ -113,7 +113,8 @@ if __name__ == '__main__':
     #G = nx.from_edgelist([(0,1),(1,2),(2,0),(0,3),(1,3),(2,3)])
     #G = nx.from_edgelist([(0,1),(0,4),(0,3),(1,5),(1,2),(2,3),(2,6),(3,7),(4,5),(4,7),(5,6),(6,7)])
     #G = nx.from_edgelist([(0,1),(0,2),(0,3),(0,4),(0,6),(1,3),(1,4),(1,2),(1,5),(2,3),(2,5),(2,6),(3,4),(3,5),(3,6)])
-    G =nx.from_edgelist([(0,1),(0,7),(0,4),(1,9),(1,2),(2,3),(2,19),(3,4),(3,17),(4,5),(5,6),(5,16),(6,7),(6,13),(7,8),(8,9),(8,12),(9,10),(10,11),(10,19),(11,12),(11,15),(12,13),(13,14),(14,15),(14,16),(15,18),(16,17),(17,18),(18,19)])
+    #G =nx.from_edgelist([(0,1),(0,7),(0,4),(1,9),(1,2),(2,3),(2,19),(3,4),(3,17),(4,5),(5,6),(5,16),(6,7),(6,13),(7,8),(8,9),(8,12),(9,10),(10,11),(10,19),(11,12),(11,15),(12,13),(13,14),(14,15),(14,16),(15,18),(16,17),(17,18),(18,19)])
+    G = nx.from_edgelist([(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(1,2),(1,3),(2,3),(2,4),(2,5),(2,6),(3,4),(4,5),(5,6)])
     upperBound = 100
     lowerBound = 0
     vars = Variables(G.nodes(), lowerBound, upperBound)
@@ -121,7 +122,8 @@ if __name__ == '__main__':
     #fixed_points = [(0,0,0),(1,2,6),(2,4,0)]
     #fixed_points = [(0,0,0),(1,0,6),(2,6,6),(3,6,0)]
     #fixed_points = [(0,0,0),(1,6,10),(2,12,0)]
-    fixed_points = [(0,0,45),(1,50,100),(2,100,45),(3,80,0),(4,20,0)]
+    #fixed_points = [(0,0,45),(1,50,100),(2,100,45),(3,80,0),(4,20,0)]
+    fixed_points = [(0,0,0),(1,50,100),(2,100,0)]
     cqm = build_cqm(vars, G, fixed_points, upperBound)
     
     best_feasible = call_solver(cqm)
@@ -129,4 +131,4 @@ if __name__ == '__main__':
     pos = create_pos_for_drawing(best_feasible[0])
     
     nx.draw(G, pos=pos, node_size=300, edgecolors='k', cmap='hsv', with_labels=True)
-    plt.savefig("tutte_draw.png")
+    plt.savefig("tutte_draw_integer_stacked.png")
--- tutte_real.py
+++ tutte_real.py
@@ -46,6 +46,21 @@ def _add_constraint(cqm: ConstrainedQuadraticModel, vars: Variables, g: nx.Graph
         cqm.add_constraint((g.degree(v)*vars.x[v]-quicksum(vars.x[u] for u in g.neighbors(v)))==0,label=f'x_constraint_node_{v}')
         cqm.add_constraint((g.degree(v)*vars.y[v]-quicksum(vars.y[u] for u in g.neighbors(v)))==0,label=f'y_constraint_node_{v}')
 
+def _define_objective(cqm: ConstrainedQuadraticModel, vars: Variables, g: nx.Graph(), no_nodes: list):
+    """objective function of the problem, minimize the distance of each point from the barycenter position
+
+    Args:
+        cqm: the model
+        vars: the x and y coordinates of the points
+        g: the graph that we want to draw
+    """
+    nodes = list(set(g.nodes)-set(no_nodes))
+    x_obj_term = quicksum((g.degree(v)*vars.x[v]-quicksum(vars.x[u] for u in g.neighbors(v))) for v in nodes)
+    y_obj_term = quicksum((g.degree(v)*vars.y[v]-quicksum(vars.y[u] for u in g.neighbors(v))) for v in nodes)
+    x_obj_coefficient = 1
+    y_obj_coefficient = 1
+    cqm.set_objective(x_obj_coefficient*x_obj_term + y_obj_coefficient*y_obj_term)
+
 def build_cqm(vars: Variables, g: nx.Graph(), fixed_points: list, upperBound: int) -> ConstrainedQuadraticModel:
     """objective function of the problem, minimize the distance of each point from the barycenter position
 
@@ -68,7 +83,7 @@ def build_cqm(vars: Variables, g: nx.Graph(), fixed_points: list, upperBound: in
         cqm.add_constraint(vars.y[el[0]] == el[2], label=f'y_constraint_node_{el[0]}')
     #print(cqm.variables)
     #print(cqm.constraints)
-    #_define_objective(cqm, vars, g, no_nodes)
+    _define_objective(cqm, vars, g, no_nodes)
     #print(cqm.variables)
     _add_constraint(cqm, vars, g, no_nodes)
     
@@ -82,7 +97,7 @@ def build_cqm(vars: Variables, g: nx.Graph(), fixed_points: list, upperBound: in
 def call_solver(cqm: ConstrainedQuadraticModel) -> SampleSet:
     sampler = LeapHybridCQMSampler()
     #4*sampler.min_time_limit(cqm),
-    res = sampler.sample_cqm(cqm, 4*sampler.min_time_limit(cqm), label="Tutte's barycenter method")
+    res = sampler.sample_cqm(cqm, label="Tutte's barycenter method")
     return res.first
 
 def create_pos_for_drawing(node_to_pos : dict) -> dict:
@@ -127,4 +142,4 @@ if __name__ == '__main__':
     
     nx.draw(G, pos=pos, node_size=300, edgecolors='k', cmap='hsv', with_labels=True)
     #print(G.edges())
-    plt.savefig("tutte_draw_real_stacked2.png")
+    plt.savefig("tutte_draw_real_stacked_with_obj_4times.png")
